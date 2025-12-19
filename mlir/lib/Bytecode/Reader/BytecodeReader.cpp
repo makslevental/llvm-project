d0308341732458cebbeeae5cdb446c49ec881d45
Maksim Levental <maksim.levental@gmail.com>
2025-12-19 11:11:41 -0800

    [mlir][bytecode] fix on macos after #172901
    `uint64_t` != `size_t` on macos (use `uint64_t` as elsewhere in this impl)

--- mlir/lib/Bytecode/Reader/BytecodeReader.cpp
+++ mlir/lib/Bytecode/Reader/BytecodeReader.cpp
@@ -1190,8 +1190,9 @@ LogicalResult AttrTypeReader::initialize(
 }
 
 template <typename T>
-T AttrTypeReader::resolveEntry(SmallVectorImpl<Entry<T>> &entries, size_t index,
-                               StringRef entryType) {
+T AttrTypeReader::resolveEntry(SmallVectorImpl<Entry<T>> &entries,
+                               uint64_t index, StringRef entryType,
+                               uint64_t depth) {
   if (index >= entries.size()) {
     emitError(fileLoc) << "invalid " << entryType << " index: " << index;
     return {};
